92eb59ffec24004a17bdf7c1841fcea764eae7fc
Chinwendu Nweje <chinwendu.nweje@pau.edu.ng>
2023-05-28 15:14:10 +0100

    learnt ownership

---
+++ Understanding-Ownership/target/debug/deps/Understanding_Ownership-10bfd266c3aeda7c.d
@@ -0,0 +1,5 @@
+C:\Users\Chinwendu Nweje\Documents\WENDY ZUCKERBERG\Learning-Rust\Understanding-Ownership\target\debug\deps\Understanding_Ownership-10bfd266c3aeda7c.rmeta: src\main.rs
+
+C:\Users\Chinwendu Nweje\Documents\WENDY ZUCKERBERG\Learning-Rust\Understanding-Ownership\target\debug\deps\Understanding_Ownership-10bfd266c3aeda7c.d: src\main.rs
+
+src\main.rs:
---
+++ Understanding-Ownership/target/debug/deps/Understanding_Ownership-f7cf557ae28fcfee.d
@@ -0,0 +1,5 @@
+C:\Users\Chinwendu Nweje\Documents\WENDY ZUCKERBERG\Learning-Rust\Understanding-Ownership\target\debug\deps\Understanding_Ownership-f7cf557ae28fcfee.rmeta: src\main.rs
+
+C:\Users\Chinwendu Nweje\Documents\WENDY ZUCKERBERG\Learning-Rust\Understanding-Ownership\target\debug\deps\Understanding_Ownership-f7cf557ae28fcfee.d: src\main.rs
+
+src\main.rs:
